9d55c75753f965c478c0e1fa75526f188d6fdecc
chandan singh tanwar <chandansinghtanwar917@gmail.com>
2022-07-26 18:15:40 +0530

    first commit

---
+++ utils.js
@@ -0,0 +1,63 @@
+let music = new Audio('./sounds/gamesound.mp3')
+
+function rectangularCollision({rectangle1,rectangle2})
+{
+    ans = rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x
+            && rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width 
+            &&  rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y
+            && rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+
+    return ans
+    
+}
+
+function determineWinner({player,enemy,timerId})
+{
+    clearTimeout(timerId)
+    document.querySelector('#displayText').style.display = 'flex'
+
+    if(player.health === enemy.health){
+        document.querySelector('#displayText').innerHTML = 'Tie Press C to play again'
+    }
+    else if(player.health > enemy.health)
+    {
+        document.querySelector('#displayText').innerHTML = 'Player 1 wins Press C to play again'
+    }
+    else if(enemy.health > player.health)
+    {
+        document.querySelector('#displayText').innerHTML = 'Player 2 wins Press C to play again'
+    }
+}
+
+
+let timer = 60
+let timerId
+function decreaseTimer()
+{
+    if(timer > 0)
+    {
+        timerId = setTimeout(decreaseTimer,1000)
+        timer--
+        document.querySelector('#timer').innerHTML = timer
+    }
+    if(timer === 0)
+    {
+       determineWinner({player,enemy,timerId})
+    }
+}
+
+
+function playsound()
+{
+    music.play()
+}
+
+function stopsound()
+{
+    music.pause()
+}
+
+function loadsound()
+{
+    music.load()
+}
